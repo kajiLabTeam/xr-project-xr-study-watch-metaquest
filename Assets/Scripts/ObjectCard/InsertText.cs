c01ae25301a256b2c501c370635cbce2655039d2
kanakanho <kahoshibakichi@gmail.com>
2024-03-09 20:55:19 +0900

    add: オブジェクトのカード,カードを並べるUI

---
+++ Assets/Scripts/ObjectCard/InsertText.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+
+public class InsertText : MonoBehaviour
+{
+    [SerializeField] public LabsState _labsState;
+    [SerializeField] public SwitchButton _switchButton;
+
+    [SerializeField] public TMP_Text _universityName;
+    [SerializeField] public TMP_Text _major;
+    [SerializeField] public TMP_Text _location;
+    [SerializeField] public TMP_Text _labName;
+
+    public string button;
+
+    public int cardNum;
+
+    void Start()
+    {
+        SetText(cardNum);
+        _switchButton.SetButtonIcon(button);
+    }
+
+    public void SetText(int n)
+    {
+        _universityName.text = _labsState.labs.objects[n].university.name;
+        _major.text = _labsState.labs.objects[n].university.undergraduate + _labsState.labs.objects[n].university.department + _labsState.labs.objects[n].university.major;
+        _location.text = _labsState.labs.objects[n].lab.location + _labsState.labs.objects[n].lab.roomNum;
+        _labName.text = _labsState.labs.objects[n].lab.name;
+    }
+}
---
+++ Assets/Scripts/ObjectCard/SwitchButton.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+public class SwitchButton : MonoBehaviour
+{
+    [SerializeField] private Canvas _searchCanvas;
+    [SerializeField] private Canvas _walkCanvas;
+    [SerializeField] private Canvas _visibilityCanvas;
+    [SerializeField] private Canvas _visibilityOffCanvas;
+
+    public bool isSearch = true;
+    public bool isWalk = false;
+    public bool isVisibility = false;
+    public bool isVisibilityOff = false;
+
+    private void Start()
+    {
+        SetIcons();
+    }
+
+    public void SetButtonIcon(string icon)
+    {
+        switch (icon)
+        {
+            case ("search"):
+                {
+                    if (isSearch) return;
+                    SetSearch();
+                    break;
+                }
+            case ("walk"):
+                {
+                    if (isWalk) return;
+                    SetWalk();
+                    break;
+                }
+            case ("visibility"):
+                {
+                    if (isVisibility) return;
+                    SetVisibility();
+                    break;
+                }
+            case ("visibilityOff"):
+                {
+                    if (isVisibilityOff) return;
+                    SetVisibilityOff();
+                    break;
+                }
+            default:
+                {
+                    if (isSearch) return;
+                    SetSearch();
+                    break;
+                }
+        }
+    }
+
+    private void SetIcons()
+    {
+        _searchCanvas.enabled = isSearch;
+        _walkCanvas.enabled = isWalk;
+        _visibilityCanvas.enabled = isVisibility;
+        _visibilityOffCanvas.enabled = isVisibilityOff;
+    }
+
+    private void SetSearch()
+    {
+        isSearch = true;
+        isWalk = false;
+        isVisibility = false;
+        isVisibilityOff = false;
+        SetIcons();
+    }
+
+    private void SetWalk()
+    {
+        isSearch = false;
+        isWalk = true;
+        isVisibility = false;
+        isVisibilityOff = false;
+        SetIcons();
+    }
+
+    private void SetVisibility()
+    {
+        isSearch = false;
+        isWalk = false;
+        isVisibility = true;
+        isVisibilityOff = false;
+        SetIcons();
+    }
+
+    private void SetVisibilityOff()
+    {
+        isSearch = false;
+        isWalk = false;
+        isVisibility = false;
+        isVisibilityOff = true;
+        SetIcons();
+    }
+}
